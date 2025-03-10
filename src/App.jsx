7161b76661a6d9c25239819b26232bc247e295e8
A H M Golam Hafiz <hasnathafiz229@gmail.com>
2025-03-10 13:08:35 -0500

    download and footer sections are implemented

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
 import Faq from "./sections/Faq";
 import Testimonials from "./sections/Testimonials";
+import Download from "./sections/Download";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -16,6 +18,8 @@ const App = () => {
         <Pricing />
         <Faq />
         <Testimonials />
+        <Download />
+        <Footer />
       </main>
     </div>
   );
