ba926dc53777e45e6b7e529ae43085bbaa4bb16e
A H M Golam Hafiz <hasnathafiz229@gmail.com>
2025-03-10 10:56:11 -0500

    hero section added, hero and header both are now responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./sections/Header";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
     <div>
       <main className="overflow-hidden">
         <Header />
+        <Hero />
       </main>
     </div>
   );
